965fedce8adf8d8ee276ba28fa540c651f6079b7
juhairulislam <juhairulislam2018@gmail.com>
2026-06-05 15:25:32 +0600

    add cors for api

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors')
 const app = express();
 const port = 5000;
 require('dotenv').config() ;
+
+app.use(cors()) ;
+
+app.use(express.json())
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
 

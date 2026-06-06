6c7fdfaf9c0202b9b69ab267f497cf8992f860f3
juhairulislam <juhairulislam2018@gmail.com>
2026-06-06 15:01:22 +0600

    add get method for my companies

--- index.js
+++ index.js
@@ -71,6 +71,17 @@ async function run() {
 
     // company related api's
 
+
+         app.get('/api/my/companies', async (req, res) => {
+            const query = {};
+            if (req.query.recruiterId) {
+                query.recruiterId = req.query.recruiterId;
+            }
+            const result = await companyCollection.findOne(query);
+
+            res.send(result || {});
+        })
+
      app.post('/api/companies' , async(req, res) =>{
 
         const company = req.body ;
